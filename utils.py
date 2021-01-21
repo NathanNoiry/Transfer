229d27cf9c8f000304f4227a685793768accd393
Nathan Noiry <noirynathan@gmail.com>
2021-01-21 14:29:11 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,12 +13,28 @@ class Generator(object):
 
     w: weight vectors used for the definition of the target variable
 
+    alpha: the true alpha parameter 
+
+    matrix_init: the initial matrix transform to be normalized
+
     z: the dataset used for the Monte-Carlo simulation ; z.shape = (mc_size,dim_features(x)+dim_target(y))
+
+    norm_factor: the normalization factor computed on z in order to renormalize the initial function func_init
+
+    matrix_normalized: matrix_init renormalized by norm_factor
+
+    radon_weights: the weights to be computed on z ; radon_weights[i] = func_normalized(alpha,z[i])
 
 
     Methods
     -------
     generate_data
+
+    compute_norm_factor
+
+    transform
+
+    compute_weights
     
     prob_source
     
@@ -26,10 +42,17 @@ class Generator(object):
 
     """
 
-    def __init__(self, mc_size, w):
+    def __init__(self, mc_size, w, alpha, matrix):
         self.mc_size = mc_size
         self.w = w
+        self.alpha = alpha
+        self.matrix_init = matrix
+
         self.z = None
+        self.norm_factor = None
+        self.matrix_normalized = None
+        self.radon_weights = None
+
 
     def generate_data(self):
         x = np.zeros((self.mc_size, 2))
@@ -41,57 +64,29 @@ class Generator(object):
         y = self.w[0] * x[:, 0] + self.w[1] * x[:, 1] + self.w[2] * x[:, 0] * x[:, 1] + eps
         self.z = np.c_[x, y]
 
+
+    def compute_norm_factor(self):
+        self.norm_factor = self.alpha.T @ np.array(list(map(self.matrix_init,self.z))).mean(axis=0) @ self.alpha
+
+        def func_norm(z):
+            return self.matrix_init(z) / self.norm_factor
+
+        self.matrix_normalized = func_norm
+
+
+    def transform(self,elem):
+        return self.alpha.T @ self.matrix_normalized(elem) @ self.alpha
+
+
+    def compute_weights(self):
+        self.radon_weights =  np.array([self.transform(elem) for elem in self.z]) * (1 / self.mc_size)
+
+
     def prob_source(self, nb_samples):
         idx = np.random.randint(self.z.shape[0], size=nb_samples)
         return self.z[idx]
 
-    def prob_target(self, nb_samples, weights):
-        idx = np.random.choice(self.z.shape[0], size=nb_samples, p=weights)
+
+    def prob_target(self, nb_samples):
+        idx = np.random.choice(self.z.shape[0], size=nb_samples, p=self.radon_weights)
         return self.z[idx]
-
-
-class Weight(object):
-        """
-    A class used to generate data
-
-    ...
-
-    Attributes
-    ----------
-
-
-    Methods
-    -------
-
-    """
-
-    def __init__(self,alpha,func):
-        self.alpha = alpha
-        self.matrix = matrix
-        self.func_init = func
-        self.weights = None
-        self.norm_factor = None
-        self.func_norm = None
-
-    def compute_weights(self,z):
-        self.norm_factor = alpha_true.T @ np.array(list(map(self.func_init,z))).mean(axis=0) @ alpha_true
-
-        def func_norm(z):
-            return self.func_init(z) / self.norm_factor
-
-        self.func_norm = func_norm
-
-    def transform(self,z):
-        return self.alpha.T @ self.func_norm(z) @ self.alpha
-
-    def 
-
-
-
-
-
-def w_matrix(z):
-    m = np.array([[z[0]**2, 0, 0],
-                 [0, 4*z[1]**2, 0],
-                 [0, 0, 2*z[2]**2]])
-    return m
