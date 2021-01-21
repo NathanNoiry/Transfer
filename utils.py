ca6f704bf7e6e2dc74a37dbdfc04771ecda122bd
Nathan Noiry <noirynathan@gmail.com>
2021-01-21 13:49:07 +0100

    Update utils.py: two classes (Generator and Weights)

--- utils.py
+++ utils.py
@@ -11,7 +11,21 @@ class Generator(object):
     ----------
     mc_size: number of samples used for the Monte-Carlo simulation
 
+    w: weight vectors used for the definition of the target variable
+
+    z: the dataset used for the Monte-Carlo simulation ; z.shape = (mc_size,dim_features(x)+dim_target(y))
+
+
+    Methods
+    -------
+    generate_data
+    
+    prob_source
+    
+    prob_target
+
     """
+
     def __init__(self, mc_size, w):
         self.mc_size = mc_size
         self.w = w
@@ -34,6 +48,46 @@ class Generator(object):
     def prob_target(self, nb_samples, weights):
         idx = np.random.choice(self.z.shape[0], size=nb_samples, p=weights)
         return self.z[idx]
+
+
+class Weight(object):
+        """
+    A class used to generate data
+
+    ...
+
+    Attributes
+    ----------
+
+
+    Methods
+    -------
+
+    """
+
+    def __init__(self,alpha,func):
+        self.alpha = alpha
+        self.matrix = matrix
+        self.func_init = func
+        self.weights = None
+        self.norm_factor = None
+        self.func_norm = None
+
+    def compute_weights(self,z):
+        self.norm_factor = alpha_true.T @ np.array(list(map(self.func_init,z))).mean(axis=0) @ alpha_true
+
+        def func_norm(z):
+            return self.func_init(z) / self.norm_factor
+
+        self.func_norm = func_norm
+
+    def transform(self,z):
+        return self.alpha.T @ self.func_norm(z) @ self.alpha
+
+    def 
+
+
+
 
 
 def w_matrix(z):
